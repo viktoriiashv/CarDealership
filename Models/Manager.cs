f796dc448f7a8ab5a615a2261a2819bef261d108
viktoriiashv <viktoriiash@tahometer.com>
2020-09-02 16:34:41 +0300

    Manager class created

---
+++ Models/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarDealership.Models
+{
+    public class Manager
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AdditionalInfo { get; set; }
+        public virtual ICollection<Manager> Managers { get; set; }
+    }
+
+}
