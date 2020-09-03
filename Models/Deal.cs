ffc381ef4e7e3f8db6b49957d7f1b761a37c2f81
viktoriiashv <viktoriiash@tahometer.com>
2020-09-03 10:34:20 +0300

    Deal class created

---
+++ Models/Deal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealership.Models
+{
+    public class Deal
+    {
+        public int ID { get; set; }
+        public int ManagerID { get; set; }
+        public int CarID { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
