fe473134893432c640a315324fe1d074cf770b7c
viktoriiashv <viktoriiash@tahometer.com>
2020-09-03 13:09:25 +0300

    Deal service added

---
+++ Services/DealService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarDealership.DAL;
+using CarDealership.Models;
+
+namespace CarDealership.Services
+{
+    public class DealService
+    {
+        CarDealershipContext db;
+
+        public DealService(CarDealershipContext db)
+        {
+            this.db = db;
+        }
+
+        public List<Deal> GetDealList()
+        {
+            return db.Deals.ToList();
+        }
+
+        public Deal AddDeal(Deal deal)
+        {
+            db.Deals.Add(deal);
+            return deal;
+        }
+    }
+}
